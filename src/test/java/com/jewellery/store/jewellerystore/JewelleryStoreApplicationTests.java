21a56b5aa1a535e565df035096f278d804cccd5a
sateesh73 <sateeshvarma73@gmail.com>
2021-05-27 21:54:27 +0530

    first commit

---
+++ src/test/java/com/jewellery/store/jewellerystore/JewelleryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewellery.store.jewellerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
